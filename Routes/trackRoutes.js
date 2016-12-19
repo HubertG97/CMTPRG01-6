41532d21d7785972a454a2d9155319e1c487b7c5
HubertG97 <0892400@hr.nl>
2016-12-19 14:20:36 +0100

    patch()
    implemented patch

--- Routes/trackRoutes.js
+++ Routes/trackRoutes.js
@@ -25,35 +25,64 @@ var routes = function(Track){
                     res.json(tracks);
             });
         });
-
+    trackRouter.use('/:trackId', function(req, res, next){
+        Track.findById(req.params.trackId, function(err,track){
+            if(err)
+                res.status(500).send(err);
+            else if (track)
+            {
+                req.track = track;
+                next();
+            }
+            else
+            {
+                res.status(404).send('No Track Found');
+            }
+        });
+    })
     trackRouter.route('/:trackId')
-
         .get(function(req,res){
-            var query = {};
+            /*var query = {};
 
             if(req.query.genre){
                 query.genre = req.query.genre;
-            }
-            Track.findById(req.params.trackId, function(err,track){
-                if(err)
-                    res.status(500).send(err);
-                else
-                    res.json(track);
-            });
+            }*/
+            res.json(req.track);
+
         })
         .put(function(req, res){
-            Track.findById(req.params.trackId, function(err,track){
+            req.track.title = req.body.title;
+            req.track.artist = req.body.artist;
+            req.track.genre = req.body.genre;
+            req.track.played = req.body.played;
+            req.track.save(function(err){
                 if(err)
                     res.status(500).send(err);
                 else
-                    track.title = req.body.title;
-                    track.artist = req.body.artist;
-                    track.genre = req.body.genre;
-                    track.played = req.body.played;
-                    track.save();
-                    res.json(track);
+                {
+                    res.json(req.track);
+                }
             });
-    });
+            res.json(req.track);
+    })
+        .patch(function(req, res){
+            if(req.body._id)
+                delete req.body._id;
+            
+            for(var p in req.body)
+            {
+                req.track[p] = req.body[p];
+            }
+
+            req.track.save(function(err){
+                if(err)
+                    res.status(500).send(err);
+                else
+                {
+                    res.json(req.track);
+                }
+            });
+        });
     return trackRouter;
 };
 
