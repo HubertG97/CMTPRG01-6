eb7858d64bb8490a9a6a58031f23f408bde664e1
HubertG97 <0892400@hr.nl>
2017-04-11 19:06:39 +0200

    prom

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
     bodyParser =  require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/trackAPI');
+mongoose.Promise = global.Promise;
 
 var Track = require('./models/trackModel');
 
